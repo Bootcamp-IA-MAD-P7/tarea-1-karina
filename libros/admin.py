09163a0d664d42116d6235d90c07f8568dec7368
karinaromerovasquez <karinaromerovasquez6@gmail.com>
2026-05-21 10:26:28 +0200

    Estructurar carpetas de templates y agregar detalle_libro.html

--- libros/admin.py
+++ libros/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Libro 
 
-admin.site.register(Libro) 
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'fecha_publicacion', 'isbn')
+    search_fields = ('titulo', 'autor', 'isbn')
+    list_filter = ('fecha_publicacion',)
